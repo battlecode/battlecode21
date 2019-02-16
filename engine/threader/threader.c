bb7f5c8ff564f53fa42d365661bf680ada8ce01e
joshuagruenstein <joshuagruenstein@gmail.com>
2019-02-15 23:43:33 -0500

    untested windows support?

--- engine/threader/threader.c
+++ engine/threader/threader.c
@@ -1,14 +1,24 @@
 #include <Python.h>
-#include <pthread.h>
+
+#if defined(_WIN32)
+	#include <windows.h>
+#else
+	#include <pthread.h>
+#endif
 
 // Forked from munawarb/Python-Kill-Thread-Extension
 
 static PyObject* threader_killThread(PyObject*, PyObject*);
-static PyObject* threader_enableCancel(PyObject*, PyObject*);
 static PyObject* threader_killThread(PyObject* self, PyObject* arg) {
 	unsigned long id = 0;
 	if (!PyArg_ParseTuple(arg, "k", &id)) return NULL;
+
+#if defined(_WIN32)
+	int succeeded = TerminateThread(id, 0);
+#else
 	int succeeded = pthread_cancel(id);
+#endif
+
 	return Py_BuildValue("i", succeeded);
 }
 
