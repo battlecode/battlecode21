116958a2ff0ef98ddee7fd1d8c0343d7fe89e9c6
Zoë Marschner <zoe.marschner@gmail.com>
2019-11-20 04:17:47 -0500

    nicer 404 page :)

--- frontend/src/views/not_found.js
+++ frontend/src/views/not_found.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 class NotFound extends Component {
+	
     render() {
+    	const soupStyle = {
+		    paddingBottom: "0",
+	    	fontSize: "100px",
+	    	marginBottom: "-30px",
+		}
+
+    	// display one of three random soup emojis :)
+    	const rand = Math.floor(Math.random() * 3)
+    	let soup_emoji = ""
+    	switch (rand) {
+    		case 0:
+    			soup_emoji = "🍲"
+    			break
+    		case 1:
+    			soup_emoji = "🥘"
+    			break
+    		case 2:
+    			soup_emoji = "🍜"
+    			break    			
+    	}
+
         return (
             <div className="content">
-                404 Not Found Content
+                <p className="text-center" style={soupStyle}> {soup_emoji} </p>
+                <h1 className="text-center">404 error</h1>
+                <h5 className="text-center">Sorry, we couldn't find the page you were looking for!</h5>
             </div>
         );
     }
