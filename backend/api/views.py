771063621fe125ddaef467f2c503c53c6e269667
Nathan <nkim8@mit.edu>
2019-12-29 10:45:03 -0800

    starting website poke to compile server pub/sub

--- backend/api/views.py
+++ backend/api/views.py
@@ -13,17 +13,67 @@ from api.serializers import *
 from api.permissions import *
 
 from google.cloud import storage
+from google.cloud import pubsub_v1
 import trueskill
 
 import os
 import tempfile, datetime
+import argparse
+import time
+
+# from pub import * # doesn't work, i think django is strange
 
 GCLOUD_PROJECT = "battlecode18" #not nessecary???
 GCLOUD_SUB_BUCKET = "bc20-submissions"
+GCLOUD_SUB_COMPILE_NAME  = 'bc20-compile-sub'
 GCLOUD_RES_BUCKET = ""
 SUBMISSION_FILENAME = lambda submission_id: f"{submission_id}/source.zip"
 RESUME_FILENAME = lambda user_id: f"{user_id}/resume.pdf"
-    
+
+
+
+# pub sub commands (from pub.py)
+def get_callback(api_future, data, ref):
+    """Wrap message data in the context of the callback function."""
+    def callback(api_future):
+        try:
+            print("Published message {} now has message ID {}".format(
+                data, api_future.result()))
+            ref["num_messages"] += 1
+        except Exception:
+            print("A problem occurred when publishing {}: {}\n".format(
+                data, api_future.exception()))
+            raise
+    return callback
+
+
+def pub(project_id, topic_name, data=""):
+    """Publishes a message to a Pub/Sub topic."""
+    # [START pubsub_quickstart_pub_client]
+    # Initialize a Publisher client.
+    client = pubsub_v1.PublisherClient()
+    # [END pubsub_quickstart_pub_client]
+    # Create a fully qualified identifier in the form of
+    # `projects/{project_id}/topics/{topic_name}`
+    topic_path = client.topic_path(project_id, topic_name)
+
+    # Data sent to Cloud Pub/Sub must be a bytestring.
+    #data = b"examplefuncs"
+    if data == "":
+        data = b"{\"gametype\":\"scrimmage\",\"gameid\":\"1\",\"player1\":\"examplefuncs\",\"player2\":\"examplefuncs\",\"maps\":\"maptestsmall\",\"replay\":\"abcdefg\"}"
+
+    # Keep track of the number of published messages.
+    ref = dict({"num_messages": 0})
+
+    # When you publish a message, the client returns a future.
+    api_future = client.publish(topic_path, data=data)
+    api_future.add_done_callback(get_callback(api_future, data, ref))
+
+    # Keep the main thread from exiting while the message future
+    # gets resolved in the background.
+    while api_future.running():
+        time.sleep(0.5)
+        print("Published {} message(s).".format(ref["num_messages"]))
 
 class GCloudUploadDownload():
     """
@@ -436,6 +486,10 @@ class SubmissionViewSet(viewsets.GenericViewSet,
         upload_url = GCloudUploadDownload.signed_upload_url(SUBMISSION_FILENAME(serializer.data['id']), GCLOUD_SUB_BUCKET)
 
         #TODO::: call to compile server
+        print('attempting call to compile server')
+        data = "{\"submissionid\":" + str(team_sub.compiling_id) + "}"
+        data_bytestring = data.encode('utf-8')
+        pub.pub(GCLOUD_PROJECT, GCLOUD_SUB_COMPILE_NAME, data)
 
         return Response({'upload_url': upload_url}, status.HTTP_201_CREATED)
 
