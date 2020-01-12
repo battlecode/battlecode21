a6da685cbd5e31529f062b74b40648beeb614fa4
Jerry Mao <j-mao@users.noreply.github.com>
2020-01-12 13:01:10 +1100

    Perform matchmaking in parallel
    Resolves #253

---
+++ infrastructure/matcher/config.py
@@ -0,0 +1,22 @@
+import os
+import logging
+
+# Configure logging format
+
+logging.basicConfig(format='%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s')
+logging.getLogger().setLevel(logging.INFO)
+
+
+# Constants, parameters and configurations
+
+API_AUTHENTICATE = 'https://2020.battlecode.org/auth/token/'
+API_USERNAME = os.getenv('BC20_DB_USERNAME')
+API_PASSWORD = os.getenv('BC20_DB_PASSWORD')
+
+API_SCRIM_LIST = 'https://2020.battlecode.org/api/match/scrimmage_list/'
+API_ENQUEUE = 'https://2020.battlecode.org/api/match/enqueue/'
+
+NUM_WORKER_THREADS = 10
+
+TOURNAMENT_SLEEP_TIME = 15
+FILE_TEAMNAMES = 'team_names'
--- infrastructure/tournament.Dockerfile
+++ infrastructure/tournament.Dockerfile
@@ -3,6 +3,6 @@ FROM bc20-env
 # Install software dependencies
 RUN pip3 install --upgrade \
     requests
-COPY * app/
 
+COPY config.py util.py bracket.py team_names tournament_server.py app/
 CMD /app/tournament_server.py
