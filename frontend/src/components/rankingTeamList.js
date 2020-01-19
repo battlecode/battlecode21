8eab648b380d7d70caf72bbf12d623e557157eb6
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2020-01-19 01:45:17 -0500

    score n/a if 0

--- frontend/src/components/rankingTeamList.js
+++ frontend/src/components/rankingTeamList.js
@@ -47,7 +47,7 @@ class RankingTeamList extends TeamList {
                 }
                 return (
                     <tr key={ team.id } onClick={() => this.showTeamPage(team.id) }>
-                            <td>{ Math.round(team.score) }</td>
+                            <td>{ team.score === 0 ? "N/A" : Math.round(team.score) }</td>
                             <td>{ team.name }</td>
                             <td>{ team.users.join(", ") }</td>
                             <td>{ team.bio }</td>
--- frontend/src/views/teamInfo.js
+++ frontend/src/views/teamInfo.js
@@ -41,7 +41,7 @@ class RankCard extends Component {
 					</div>
 					<br></br>
 					<p style={{textAlign: 'center'}}>
-						Score: {Math.round(this.props.team.score)}
+						Score: {this.props.team.score === 0 ? "N/A" : Math.round(this.props.team.score)}
 					</p>
 				</div>
 			</div>
