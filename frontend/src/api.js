2e362b739cd00d33fe613e737e8cea837b9bcecf
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2020-01-21 20:08:17 -0500

    next tournament is intl quals

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -534,10 +534,15 @@ class Api {
   static getNextTournament(callback) {
     // TODO: actually use real API for this
     callback({
-      "est_date_str": '7 PM EST on January 20, 2020',
-      "seconds_until": (Date.parse(new Date('January 20, 2020 19:00:00')) - Date.parse(new Date())) / 1000,
-      "tournament_name": "Seeding Tournament"
+      "est_date_str": '7 PM EST on January 23, 2020',
+      "seconds_until": (Date.parse(new Date('January 23, 2020 19:00:00')) - Date.parse(new Date())) / 1000,
+      "tournament_name": "International Qualifying Tournament"
     });
+    // callback({
+    //   "est_date_str": '7 PM EST on January 20, 2020',
+    //   "seconds_until": (Date.parse(new Date('January 20, 2020 19:00:00')) - Date.parse(new Date())) / 1000,
+    //   "tournament_name": "Seeding Tournament"
+    // });
     // callback({
     //   "est_date_str": '7 PM EST on January 6, 2020',
     //   "seconds_until": (Date.parse(new Date('January 6, 2020 19:00:00')) - Date.parse(new Date())) / 1000,
