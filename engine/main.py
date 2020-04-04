ca1f24c662de61e2f703d04f3cd8b0a007f6b700
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2020-04-04 14:39:19 +0200

    convenience multiple bots run

--- engine/main.py
+++ engine/main.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 import time
+import argparse
 
 from battlecode.engine.container.code_container import CodeContainer
 from battlecode.engine.game.game import Game
@@ -8,11 +10,16 @@ import faulthandler
 
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('player', nargs='+', help="Player")
+    args = parser.parse_args()
+
     faulthandler.enable()
 
-    code_container = CodeContainer.from_directory('./examplefuncsplayer')
+    code_container1 = CodeContainer.from_directory(args.player[0])
+    code_container2 = CodeContainer.from_directory(args.player[1] if len(args.player) > 1 else args.player[0])
 
-    game = Game([code_container, code_container], debug=True)
+    game = Game([code_container1, code_container2], debug=True)
 
     start = time.time()
     print(f'Start Time: {start}')
