0f5cc954f2d0aaf42d1d75a6f63d3658d1a39058
Anne Ouyang <anne.ouyang@gmail.com>
2020-04-12 22:29:28 -0400

    Update 404 page

--- frontend/src/views/not_found.js
+++ frontend/src/views/not_found.js
@@ -4,40 +4,40 @@ class NotFound extends Component {
 	constructor(props) {
 		super()
 		this.state = {
-			soup: ''
+			pawn: '♟️'
 		}
 	}
 
 	render() {
-		const soupStyle = {
+		const pawnStyle = {
 			paddingBottom: "0",
 			fontSize: "100px",
 			marginBottom: "-30px",
 		}
 
 		// display one of three random soup emojis :)
-		if (!this.state.soup) {
-			const rand = Math.floor(Math.random() * 3)
-			let soup_emoji = ""
-			switch (rand) {
-				case 0:
-					soup_emoji = "🍲"
-					break
-				case 1:
-					soup_emoji = "🥘"
-					break
-				default:
-					soup_emoji = "🍜"
+		// if (!this.state.soup) {
+		// 	const rand = Math.floor(Math.random() * 3)
+		// 	let soup_emoji = ""
+		// 	switch (rand) {
+		// 		case 0:
+		// 			soup_emoji = "🍲"
+		// 			break
+		// 		case 1:
+		// 			soup_emoji = "🥘"
+		// 			break
+		// 		default:
+		// 			soup_emoji = "🍜"
        
-			}
+		// 	}
 
-			this.setState({soup: soup_emoji})
-		}
+		// 	this.setState({soup: soup_emoji})
+		// }
 		
 
 		return (
 			<div className="content">
-				<p className="text-center" style={soupStyle}> {this.state.soup} </p>
+				<p className="text-center" style={pawnStyle}> {this.state.pawn} </p>
 				<h1 className="text-center">404 error</h1>
 				<h5 className="text-center">Sorry, we couldn't find the page you were looking for!</h5>
 			</div>
