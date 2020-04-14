35fdfb86c86273eda202ae2cb9923155083830ef
Stephanie Fu <fus@mit.edu>
2020-04-13 23:24:34 -0500

    chess theme

--- frontend/src/sidebar.js
+++ frontend/src/sidebar.js
@@ -52,10 +52,10 @@ class SideBar extends Component {
 
     render() {
         return (
-            <div className="sidebar" data-color="space"> {/* data-color is defined in light-bootstrap-dashboard.css */}
+            <div className="sidebar" data-color="chess"> {/* data-color is defined in light-bootstrap-dashboard.css */}
                 <div className="sidebar-wrapper">
                     <div className="logo">
-                        <a href="#" className="simple-text">Battlehack</a>
+                        <img src="../assets/img/logo.png" />
                     </div>
                     <ul className="nav nav-pills nav-stacked">
                         <NLink to={`${process.env.PUBLIC_URL}/home`}><p style={{fontWeight: "inherit", textTransform: "none", fontSize: "inherit"}}><i className="pe-7s-home pe-fw" />Home</p></NLink>
