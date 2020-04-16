9b49c9f25f272e32c45fd4fe673574cacf5f7bc5
Nathaniel Kim <nkim8@mit.edu>
2020-04-16 09:08:16 -0700

    change max team size to 2

--- backend/api/views.py
+++ backend/api/views.py
@@ -586,7 +586,7 @@ class TeamViewSet(viewsets.GenericViewSet,
             return Response({'message': 'Already on a team in this league'}, status.HTTP_400_BAD_REQUEST)
         if team.team_key != request.data.get('team_key', None):
             return Response({'message': 'Invalid team key'}, status.HTTP_400_BAD_REQUEST)
-        if team.users.count() == 4:
+        if team.users.count() == 2:
             return Response({'message': 'Team has max number of users'}, status.HTTP_400_BAD_REQUEST)
         team.users.add(request.user.id)
         team.save()
