831e7187d64068edf6a6ab00dcf4c82315dc6e7e
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2020-04-16 20:32:35 +0200

    make color an instance variable of the viewer

--- engine/battlehack20/engine/game/viewer.py
+++ engine/battlehack20/engine/game/viewer.py
@@ -4,9 +4,10 @@ import sys
 from .team import Team
 
 class BasicViewer:
-    def __init__(self, board_size, board_states):
+    def __init__(self, board_size, board_states, colors=True):
         self.board_size = board_size
         self.board_states = board_states
+        self.colors = colors
 
     def play(self, delay=0.5):
         print('Visualizer: ')
@@ -26,19 +27,19 @@ class BasicViewer:
     def view(self, index=-1):
         print(self.view_board(self.board_states[index]))
 
-    def view_board(self, board, colors=True):
+    def view_board(self, board):
         new_board = ''
         for i in range(self.board_size):
             for j in range(self.board_size):
                 if board[i][j]:
                     new_board += '['
-                    if colors:
+                    if self.colors:
                         if board[i][j].team == Team.WHITE:
                             new_board += '\033[1m\u001b[37m'
                         else:
                             new_board += '\033[1m\u001b[36m'
                     new_board += str(board[i][j])
-                    if colors:
+                    if self.colors:
                         new_board += '\033[0m\u001b[0m] '
                 else:
                     new_board += '[    ] '
