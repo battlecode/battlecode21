4f4eafcf80fe21a0ad0592c96f1da014e3494db8
Elizabeth Zou <ezou@mit.edu>
2020-11-08 16:45:23 -0600

    Added schema folder

---
+++ schema/ts/index.ts
@@ -0,0 +1,7 @@
+import { battlecode as bc } from "./battlecode_generated";
+import schema = bc.schema;
+export { schema };
+
+export { flatbuffers } from 'flatbuffers';
+
+// export { battlecode.schema as schema } from './battlecode_generated';
