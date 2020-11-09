dca04fd242f4c04d1db42db39b2eff3a88dd15cf
Elizabeth Zou <ezou@mit.edu>
2020-11-08 20:02:05 -0600

    Removed MIN_WATER_ELEVATION

--- engine/src/main/battlecode/common/GameConstants.java
+++ engine/src/main/battlecode/common/GameConstants.java
@@ -27,10 +27,6 @@ public class GameConstants {
     /** The maximum possible map width. */
     public static final int MAP_MAX_WIDTH = 64;
 
-    /** The elevation of the lowest tile that has water in it on a map.
-     * Every map needs to have a tile with exactly this elevation in the beginning. **/
-    public static final int MIN_WATER_ELEVATION = Integer.MIN_VALUE/2;
-
     // *********************************
     // ****** GAME PARAMETERS **********
     // *********************************
