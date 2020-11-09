5c1aa9bdb46e5255610338e8f8c4efe6c177f80b
Elizabeth Zou <ezou@mit.edu>
2020-11-09 00:36:02 -0600

    Random changes to RobotControllerImpl, might come back and just delete things (will no longer run)

--- engine/src/main/battlecode/world/RobotControllerImpl.java
+++ engine/src/main/battlecode/world/RobotControllerImpl.java
@@ -238,6 +238,7 @@ public final strictfp class RobotControllerImpl implements RobotController {
         return validSensedRobots.toArray(new RobotInfo[validSensedRobots.size()]);
     }
 
+    // TODO: delete
     @Override
     public MapLocation adjacentLocation(Direction dir) {
         return getLocation().add(dir);
