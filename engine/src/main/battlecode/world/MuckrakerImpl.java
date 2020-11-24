3c16ba8a69d532714c7901f00a430be3670a87b1
unknown <debelen.arthurreiner@gmail.com>
2020-11-24 16:36:37 -0500

    skeletons for impls

---
+++ engine/src/main/battlecode/world/MuckrakerImpl.java
@@ -0,0 +1,34 @@
+package battlecode.world;
+
+import battlecode.common.*;
+import static battlecode.common.GameActionExceptionType.*;
+import battlecode.instrumenter.RobotDeathException;
+import battlecode.schema.Action;
+
+import java.util.*;
+
+/**
+ * The implementation of PoliticianController, a subclass of RobotController. Its methods *must* be called
+ * from a player thread.
+ *
+ * It is theoretically possible to have multiple for a single InternalRobot, but
+ * that may cause problems in practice, and anyway why would you want to?
+ *
+ * All overriden methods should assertNotNull() all of their (Object) arguments,
+ * if those objects are not explicitly stated to be nullable.
+ */
+
+ 
+public final strictfp class MuckrakerImpl extends RobotController {
+
+    /**
+     * Create a new RobotControllerImpl
+     *
+     * @param gameWorld the relevant world
+     * @param robot the relevant robot
+     */
+    public MuckrakerImpl(GameWorld gameWorld, InternalRobot robot) {
+        super(gameWorld, robot); 
+    }
+
+}
