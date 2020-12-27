a52f8e8243189d34f08a5a14f9f87763274e3444
Nathaniel Kim <nkim8@mit.edu>
2020-12-26 21:19:13 -0800

    Add basic middleware

--- backend/api/email_helpers.py
+++ backend/api/email_helpers.py
@@ -10,3 +10,18 @@ def send_email(recipient, subject, content, is_html):
         response = sg.send(message)
     except Exception as e:
         print(str(e))
+
+# Middleware that sends an email on exceeptions.
+# https://docs.djangoproject.com/en/2.2/topics/http/middleware/
+# https://simpleisbetterthancomplex.com/tutorial/2016/07/18/how-to-create-a-custom-django-middleware.html
+class EmailMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_exception(self, request, exception):
+        print("exception")
+        return None
--- backend/settings.py
+++ backend/settings.py
@@ -132,6 +132,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'api.email_helpers.EmailMiddleware'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
