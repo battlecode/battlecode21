ffcbc54abd1402b9b0ee8170e510f9fecf0eee87
Stephanie Fu <fus@mit.edu>
2020-12-28 13:16:17 -0600

    Update submission text for Java

--- frontend/src/views/submissions.js
+++ frontend/src/views/submissions.js
@@ -233,17 +233,14 @@ class Submissions extends Component {
                             We will have a 5-minute grace period; if you're having trouble submitting, send us your code on Discord before 7:05. If the code you submit to us on Discord has only minor differences to the code submitted on time through the website (e.g., 1 or 2 lines), we will accept it. <b>We will not accept anything submitted after 7:05 pm.</b>
                         </p> */}
                         <p>
-                        Create a <code>zip</code> file of your robot player using the <b>utility found <a href="https://github.com/battlecode/battlehack20-scaffold/blob/master/zipper.py">here</a></b>. Download this file and place it alongside your <code>run.py</code>. Use it with <code>[python/python3] zipper.py [file location of your bot, e.g. examplefuncsplayer]</code>. The utility will give you feedback if you have entered an incorrect file location. Once you have your <code>zip</code> file, upload it here. <b>Please</b> use this utility; <b>incorrectly structured submissions will not run</b>. Submit the <code>zip</code> file below.
+                            Create a <code>zip</code> file of your robot player. The <code>zip</code> file can only contain 1 player package, and needs to have a <code>RobotPlayer.java</code> file. Submit the <code>zip</code> file below. Ensure that you're not importing any packages not included in the <code>zip</code> file, or your code won't compile.
                         </p>
-
-                        <p>
-                        Please <b><i>stay on this page until the card below indicates success.</i></b> To double-check that your code has been submitted, you can download at "Latest Submissions".
-
-                        </p>
-                        {file_button}
-                        {file_button_2}
+                        <label htmlFor="file_upload">
+                            <div className="btn"> Choose File </div> <span style={ { textTransform: 'none', marginLeft: '10px', fontSize: '14px'} }> {file_label} </span>
+                        </label>
+                        <input id="file_upload" type="file" accept=".zip" onChange={this.onChangeHandler} style={{display: "none"}}/>
                         {button}
-                        {/* <p id="sub_status" className="text-center category"> {status_str}</p> */}
+                        <p className="text-center category"> {status_str}</p>
                     </div>
                 </div>
             )
