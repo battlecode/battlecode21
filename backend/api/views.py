a512c1d644aaff8bcd9d1eeb2e8040c9ecda7cd4
Nathaniel Kim <nkim8@mit.edu>
2021-01-04 04:26:03 -0800

    actually pub to pubsub once more

--- backend/api/views.py
+++ backend/api/views.py
@@ -168,9 +168,7 @@ def scrimmage_pub_sub_call(red_submission_id, blue_submission_id, red_team_name,
         'replay': scrimmage_replay
     }
     data_bytestring = json.dumps(scrimmage_server_data).encode('utf-8')
-    # TODO uncomment this and remove the print when done testing; avoiding flooding the pubsub for now
-    # pub(GCLOUD_PROJECT, GCLOUD_SUB_SCRIMMAGE_NAME, data_bytestring)
-    print("would add to pubsub: ", data_bytestring)
+    pub(GCLOUD_PROJECT, GCLOUD_SUB_SCRIMMAGE_NAME, data_bytestring)
 
 def get_random_maps(num):
     n = min(num, len(settings.SERVER_MAPS))
