1228b1d2be30b62daa67b6a4cbe3295caa8f567e
Jasper van Merle <jaspervmerle@gmail.com>
2021-01-09 22:13:26 +0100

    Limit the amount of recorded profiling events

--- engine/src/main/battlecode/instrumenter/profiler/ProfilerEvent.java
+++ engine/src/main/battlecode/instrumenter/profiler/ProfilerEvent.java
@@ -1,25 +1,25 @@
 package battlecode.instrumenter.profiler;
 
 public class ProfilerEvent {
-  private ProfilerEventType type;
-  private int at;
-  private int frameId;
+    private ProfilerEventType type;
+    private int at;
+    private int frameId;
 
-  public ProfilerEvent(ProfilerEventType type, int at, int frameId) {
-    this.type = type;
-    this.at = at;
-    this.frameId = frameId;
-  }
+    public ProfilerEvent(ProfilerEventType type, int at, int frameId) {
+        this.type = type;
+        this.at = at;
+        this.frameId = frameId;
+    }
 
-  public ProfilerEventType getType() {
-    return type;
-  }
+    public ProfilerEventType getType() {
+        return type;
+    }
 
-  public int getAt() {
-    return at;
-  }
+    public int getAt() {
+        return at;
+    }
 
-  public int getFrameId() {
-    return frameId;
-  }
+    public int getFrameId() {
+        return frameId;
+    }
 }
